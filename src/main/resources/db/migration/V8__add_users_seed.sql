b68011685b6029e2362a528e631e7c9f1124d20b
jbenkodakovic <josip.benkodakovic@barrage.net>
2024-09-25 14:55:26 +0200

    Update migrations and separate seeds from migrations

--- src/main/resources/db/migration/V8__add_users_seed.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO users (email, first_name, last_name, role, default_agent_id, active)
-VALUES
-('antonio.vidakovic@barrage.net', 'Antonio', 'V', 'ADMIN', 1, TRUE),
-('filip.brkic@barrage.net', 'Filip', 'B', 'ADMIN', 1, TRUE),
-('dino@barrage.net', 'Dino', 'B', 'ADMIN', 1, TRUE);
