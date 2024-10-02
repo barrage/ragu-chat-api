71067767bff2b626a15b7b9f2a3c9ef8b853b260
jbenkodakovic <josip.benkodakovic@barrage.net>
2024-10-02 09:12:05 +0200

    Update agent controllers to use pagination DTOs

--- src/main/kotlin/net/barrage/llmao/models/Agent.kt
+++ src/main/kotlin/net/barrage/llmao/models/Agent.kt
@@ -43,46 +43,14 @@ data class CreateAgent(val name: String, val context: String) {
   fun validate(): ValidationResult {
     val errors: MutableList<String> = mutableListOf()
 
-    if (name.length < 3) {
-      errors.add("Agent name must be at least 3 characters long")
-    }
-
-    if (name.length > 255) {
-      errors.add("Agent name is too long. Max 255 characters")
-    }
-
-    if (context.length < 20) {
-      errors.add("Agent context must be at least 20 characters long")
-    }
-
-    if (errors.isNotEmpty()) {
-      return ValidationResult.Invalid(errors)
-    }
-
     return ValidationResult.Valid
   }
 }
 
 @Serializable
-data class UpdateAgent(val name: String, val context: String) {
+data class UpdateAgent(val name: String? = null, val context: String? = null) {
   fun validate(): ValidationResult {
     val errors: MutableList<String> = mutableListOf()
-
-    if (name.length < 3) {
-      errors.add("Agent name must be at least 3 characters long")
-    }
-
-    if (name.length > 255) {
-      errors.add("Agent name is too long. Max 255 characters")
-    }
-
-    if (context.length < 20) {
-      errors.add("Agent context must be at least 20 characters long")
-    }
-
-    if (errors.isNotEmpty()) {
-      return ValidationResult.Invalid(errors)
-    }
 
     return ValidationResult.Valid
   }
