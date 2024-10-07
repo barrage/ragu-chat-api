92485e0fb924412c980363b31e07e73af1a721e4
Dino Bajramovic <dino@barrage.net>
2024-10-07 11:44:50 +0200

    Update Google OAuth login error handling

--- src/main/kotlin/net/barrage/llmao/app/auth/google/GoogleAuthenticationProvider.kt
+++ src/main/kotlin/net/barrage/llmao/app/auth/google/GoogleAuthenticationProvider.kt
@@ -5,11 +5,13 @@ import io.ktor.client.*
 import io.ktor.client.call.*
 import io.ktor.client.request.*
 import io.ktor.client.request.forms.*
+import io.ktor.client.statement.*
 import io.ktor.http.*
 import net.barrage.llmao.core.auth.AuthenticationProvider
 import net.barrage.llmao.core.auth.LoginPayload
 import net.barrage.llmao.core.auth.UserInfo
 import net.barrage.llmao.error.apiError
+import net.barrage.llmao.utils.Logger
 
 class GoogleAuthenticationProvider(
   private val client: HttpClient,
@@ -33,6 +35,16 @@ class GoogleAuthenticationProvider(
       }
 
     val res = client.submitForm(url = tokenEndpoint, params, encodeInQuery = false)
+
+    if (res.status != HttpStatusCode.OK) {
+      val errorBody = res.bodyAsText()
+      Logger.error("Failed to retrieve token: ${res.status}. Response: $errorBody")
+      throw apiError(
+        "Authentication",
+        "Failed to retrieve token: ${res.status}. Response: $errorBody",
+      )
+    }
+
     val response = res.body<GoogleTokenResponse>()
     val idToken = JWT.decode(response.idToken)
 
