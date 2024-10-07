88ab537d999a89096ee6c5fbfd08c891e6ca6ce7
jbenkodakovic <josip.benkodakovic@barrage.net>
2024-10-07 13:35:32 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,8 +4,22 @@ ln -f hooks/pre-push .git/hooks/
 # Set up the docker containers
 docker compose up -d
 
-# Set up the gradle properties
-cp gradle.example.properties gradle.properties
+# Set up the gradle properties if not already present
+cat gradle.properties &> /dev/null
+if [ $? -eq 0 ]; then
+  echo "gradle.properties already exists, skipping"
+else
+  cp gradle.example.properties gradle.properties
+fi
+
 
 # Set up the application properties
-cp ./config/application.example.yaml ./config/application.yaml
+cat ./config/application.yaml &> /dev/null
+
+if [ $? -eq 0 ]; then
+  echo "application.yaml already exists, skipping"
+  exit 0;
+else
+  cp ./config/application.example.yaml ./config/application.yaml
+fi
+
