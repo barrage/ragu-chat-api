049933c87c526d66241cb0eaf6b1d2e57b22a2d6
jbenkodakovic <josip.benkodakovic@barrage.net>
2024-10-08 13:51:31 +0200

    Add administration service
    Adds administration service for various metadata and app info
    Adds LLM listing route

---
+++ src/main/kotlin/net/barrage/llmao/app/api/http/controllers/AdministrationController.kt
@@ -0,0 +1,16 @@
+package net.barrage.llmao.app.api.http.controllers
+
+import io.ktor.http.*
+import io.ktor.server.application.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+import net.barrage.llmao.core.services.AdministrationService
+
+fun Route.administrationRouter(service: AdministrationService) {
+  get("/admin/providers/llm/{provider}") {
+    // Safe to yell since the route won't match if there's no provider
+    val provider = call.parameters["provider"]!!
+    val models = service.listLanguageModels(provider)
+    call.respond(HttpStatusCode.OK, models)
+  }
+}
