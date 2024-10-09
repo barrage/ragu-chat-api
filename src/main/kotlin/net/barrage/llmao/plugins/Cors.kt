ba58850af2eef2ab858a6bb4f353e8df5f04a735
jbenkodakovic <josip.benkodakovic@barrage.net>
2024-10-09 11:43:39 +0200

    Update CORS to allow credentials

--- src/main/kotlin/net/barrage/llmao/plugins/Cors.kt
+++ src/main/kotlin/net/barrage/llmao/plugins/Cors.kt
@@ -8,12 +8,11 @@ fun Application.configureCors() {
   val origins = environment.config.property("cors.origins").getList()
   val methods = environment.config.property("cors.methods").getList().map { HttpMethod(it) }
   val headers = environment.config.property("cors.headers").getList()
-  val maxAgeInSeconds: Long = environment.config.property("cors.maxAge").getString().toLong()
 
   install(CORS) {
+    this.allowCredentials = true
     this.allowOrigins { origins.contains(it) }
     this.methods.addAll(methods)
     this.headers.addAll(headers)
-    this.maxAgeInSeconds = maxAgeInSeconds
   }
 }
