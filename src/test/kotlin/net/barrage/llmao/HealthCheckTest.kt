8b0367f0e52e5ba3d4e6cd21b21e14e8c1c4a7ac
jbenkodakovic <josip.benkodakovic@barrage.net>
2024-10-11 13:22:48 +0200

    Update TestClass name to IntegrationTest

--- src/test/kotlin/net/barrage/llmao/HealthCheckTest.kt
+++ src/test/kotlin/net/barrage/llmao/HealthCheckTest.kt
@@ -4,7 +4,7 @@ import io.ktor.client.request.*
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
-class HealthCheckTest : TestClass(usePostgres = false) {
+class HealthCheckTest : IntegrationTest(usePostgres = false) {
   @Test
   fun testHealthCheck() = test {
     val res = client.get("__health")
