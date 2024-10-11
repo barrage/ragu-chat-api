de04ca631017289b916366abb0b830b2aef7ecb0
Antonio Vidaković <antonio.vidakovic@barrage.net>
2024-10-11 09:22:39 +0200

    Fix for AdminUserControllerTests make fixup before merge

--- src/test/kotlin/net/barrage/llmao/app/api/http/controllers/AdminUserControllerTests.kt
+++ src/test/kotlin/net/barrage/llmao/app/api/http/controllers/AdminUserControllerTests.kt
@@ -3,7 +3,6 @@ package net.barrage.llmao.app.api.http.controllers
 import io.ktor.client.call.*
 import io.ktor.client.plugins.contentnegotiation.*
 import io.ktor.client.request.*
-import io.ktor.client.statement.*
 import io.ktor.serialization.kotlinx.json.*
 import java.util.*
 import kotlin.test.assertEquals
@@ -23,22 +22,22 @@ class AdminUserControllerTests : TestClass() {
 
   @Test
   fun listAllUsersDefaultPagination() = test {
-    createClient { install(ContentNegotiation) { json() } }
-      .get("admin/users") { header("Cookie", sessionCookie(adminSession.sessionId)) }
-      .apply {
-        assertEquals(200, status.value)
-        val result: CountedList<User> = body()!!
-        assertEquals(2, result.total)
-        assertTrue { result.items.any { it.id == adminUser.id } }
-        assertTrue { result.items.any { it.id == peasantUser.id } }
-      }
+    val client = createClient { install(ContentNegotiation) { json() } }
+    val response =
+      client.get("admin/users") { header("Cookie", sessionCookie(adminSession.sessionId)) }
+
+    assertEquals(200, response.status.value)
+    val body: CountedList<User> = response.body()!!
+    assertEquals(2, body.total)
+    assertTrue { body.items.any { it.id == adminUser.id } }
+    assertTrue { body.items.any { it.id == peasantUser.id } }
   }
 
   @Test
   fun createUserSuccess() = test {
     val client = createClient { install(ContentNegotiation) { json() } }
-    client
-      .post("admin/users") {
+    val response =
+      client.post("admin/users") {
         header("Cookie", sessionCookie(adminSession.sessionId))
         header("Content-Type", "application/json")
         setBody(
@@ -51,23 +50,22 @@ class AdminUserControllerTests : TestClass() {
           )
         )
       }
-      .apply {
-        assertEquals(201, status.value)
-        val result: User = body()!!
-        assertEquals("create@user.ban", result.email)
-        assertEquals("Create User", result.fullName)
-        assertEquals("Create", result.firstName)
-        assertEquals("User", result.lastName)
-        assertEquals("USER", result.role)
-        postgres!!.deleteTestUser(result.id)
-      }
+
+    assertEquals(201, response.status.value)
+    val body: User = response.body()!!
+    assertEquals("create@user.ban", body.email)
+    assertEquals("Create User", body.fullName)
+    assertEquals("Create", body.firstName)
+    assertEquals("User", body.lastName)
+    assertEquals("USER", body.role)
+    postgres!!.deleteTestUser(body.id)
   }
 
   @Test
   fun createUserValidationFail() = test {
     val client = createClient { install(ContentNegotiation) { json() } }
-    client
-      .post("admin/users") {
+    val response =
+      client.post("admin/users") {
         header("Cookie", sessionCookie(adminSession.sessionId))
         header("Content-Type", "application/json")
         setBody(
@@ -80,34 +78,31 @@ class AdminUserControllerTests : TestClass() {
           }"""
         )
       }
-      .apply {
-        println(bodyAsText())
-        assertEquals(422, status.value)
-        val body: List<ValidationError> = body()!!
-        assertEquals(4, body.size)
-        body.forEach {
-          println(it)
-          when (it.fieldName) {
-            "email" -> {
-              assertEquals("email", it.code)
-              assertEquals("email", it.fieldName)
-              assertEquals("Value is not valid email", it.message)
-            }
 
-            else -> {
-              assertEquals("notBlank", it.code)
-              assertEquals("Value cannot be blank", it.message)
-            }
-          }
+    assertEquals(422, response.status.value)
+    val body: List<ValidationError> = response.body()!!
+    assertEquals(4, body.size)
+    body.forEach {
+      when (it.fieldName) {
+        "email" -> {
+          assertEquals("email", it.code)
+          assertEquals("email", it.fieldName)
+          assertEquals("Value is not valid email", it.message)
+        }
+
+        else -> {
+          assertEquals("notBlank", it.code)
+          assertEquals("Value cannot be blank", it.message)
         }
       }
+    }
   }
 
   @Test
   fun createUserFailsExistingEmail() = test {
     val client = createClient { install(ContentNegotiation) { json() } }
-    client
-      .post("admin/users") {
+    val response =
+      client.post("admin/users") {
         header("Cookie", sessionCookie(adminSession.sessionId))
         header("Content-Type", "application/json")
         setBody(
@@ -120,53 +115,49 @@ class AdminUserControllerTests : TestClass() {
           )
         )
       }
-      .apply {
-        assertEquals(409, status.value)
-        val body: AppError = body()!!
-        assertEquals(ErrorReason.EntityAlreadyExists, body.reason)
-      }
+
+    assertEquals(409, response.status.value)
+    val body: AppError = response.body()!!
+    assertEquals(ErrorReason.EntityAlreadyExists, body.reason)
   }
 
   @Test
   fun getSingleUser() = test {
     val client = createClient { install(ContentNegotiation) { json() } }
-    client
-      .get("admin/users/${adminUser.id}") {
+    val response =
+      client.get("admin/users/${adminUser.id}") {
         header("Cookie", sessionCookie(adminSession.sessionId))
       }
-      .apply {
-        assertEquals(200, status.value)
-        val result: User = body()!!
-        assertEquals(adminUser.id, result.id)
-        assertEquals(adminUser.email, result.email)
-        assertEquals(adminUser.fullName, result.fullName)
-        assertEquals(adminUser.firstName, result.firstName)
-        assertEquals(adminUser.lastName, result.lastName)
-        assertEquals(adminUser.role, result.role)
-      }
+
+    assertEquals(200, response.status.value)
+    val result: User = response.body()!!
+    assertEquals(adminUser.id, result.id)
+    assertEquals(adminUser.email, result.email)
+    assertEquals(adminUser.fullName, result.fullName)
+    assertEquals(adminUser.firstName, result.firstName)
+    assertEquals(adminUser.lastName, result.lastName)
+    assertEquals(adminUser.role, result.role)
   }
 
   @Test
   fun getSingleUserFailsUserNotFound() = test {
     val client = createClient { install(ContentNegotiation) { json() } }
-    client
-      .get("admin/users/${UUID.randomUUID()}") {
+    val response =
+      client.get("admin/users/${UUID.randomUUID()}") {
         header("Cookie", sessionCookie(adminSession.sessionId))
       }
-      .apply {
-        assertEquals(404, status.value)
-        val body: AppError = body()!!
-        assertEquals(ErrorReason.EntityDoesNotExist, body.reason)
-      }
+
+    assertEquals(404, response.status.value)
+    val body: AppError = response.body()!!
+    assertEquals(ErrorReason.EntityDoesNotExist, body.reason)
   }
 
   @Test
   fun updateUserSuccess() = test {
     val testUser = postgres!!.testUser("update@user.net", admin = true, active = false)
-    println("Test user: $testUser")
     val client = createClient { install(ContentNegotiation) { json() } }
-    client
-      .put("admin/users/${peasantUser.id}") {
+    val response =
+      client.put("admin/users/${peasantUser.id}") {
         header("Cookie", sessionCookie(adminSession.sessionId))
         header("Content-Type", "application/json")
         setBody(
@@ -180,26 +171,25 @@ class AdminUserControllerTests : TestClass() {
           }"""
         )
       }
-      .apply {
-        println(bodyAsText())
-        assertEquals(200, status.value)
-        val result: User = body()!!
-        assertEquals(peasantUser.id, result.id)
-        assertEquals("updated@user.com", result.email)
-        assertEquals("Updated User", result.fullName)
-        assertEquals("Updated", result.firstName)
-        assertEquals("User", result.lastName)
-        assertEquals("USER", result.role)
-        assertTrue(result.active)
-      }
+
+    assertEquals(200, response.status.value)
+    val result: User = response.body()!!
+    assertEquals(peasantUser.id, result.id)
+    assertEquals("updated@user.com", result.email)
+    assertEquals("Updated User", result.fullName)
+    assertEquals("Updated", result.firstName)
+    assertEquals("User", result.lastName)
+    assertEquals("USER", result.role)
+    assertTrue(result.active)
+
     postgres.deleteTestUser(testUser.id)
   }
 
   @Test
   fun updateUserFailsUserNotFound() = test {
     val client = createClient { install(ContentNegotiation) { json() } }
-    client
-      .put("admin/users/${UUID.randomUUID()}") {
+    val response =
+      client.put("admin/users/${UUID.randomUUID()}") {
         header("Cookie", sessionCookie(adminSession.sessionId))
         header("Content-Type", "application/json")
         setBody(
@@ -213,18 +203,17 @@ class AdminUserControllerTests : TestClass() {
           }"""
         )
       }
-      .apply {
-        assertEquals(404, status.value)
-        val body: AppError = body()!!
-        assertEquals(ErrorReason.EntityDoesNotExist, body.reason)
-      }
+
+    assertEquals(404, response.status.value)
+    val body: AppError = response.body()!!
+    assertEquals(ErrorReason.EntityDoesNotExist, body.reason)
   }
 
   @Test
   fun updateUserFailsValidation() = test {
     val client = createClient { install(ContentNegotiation) { json() } }
-    client
-      .put("admin/users/${UUID.randomUUID()}") {
+    val reponse =
+      client.put("admin/users/${UUID.randomUUID()}") {
         header("Cookie", sessionCookie(adminSession.sessionId))
         header("Content-Type", "application/json")
         setBody(
@@ -238,49 +227,48 @@ class AdminUserControllerTests : TestClass() {
           }"""
         )
       }
-      .apply {
-        assertEquals(422, status.value)
-        val body: List<ValidationError> = body()!!
-        assertEquals(4, body.size)
-        body.forEach {
-          when (it.fieldName) {
-            "email" -> {
-              assertEquals("email", it.code)
-              assertEquals("email", it.fieldName)
-              assertEquals("Value is not valid email", it.message)
-            }
 
-            else -> {
-              assertEquals("notBlank", it.code)
-              assertEquals("Value cannot be blank", it.message)
-            }
-          }
+    assertEquals(422, reponse.status.value)
+    val body: List<ValidationError> = reponse.body()!!
+    assertEquals(4, body.size)
+    body.forEach {
+      when (it.fieldName) {
+        "email" -> {
+          assertEquals("email", it.code)
+          assertEquals("email", it.fieldName)
+          assertEquals("Value is not valid email", it.message)
+        }
+
+        else -> {
+          assertEquals("notBlank", it.code)
+          assertEquals("Value cannot be blank", it.message)
         }
       }
+    }
   }
 
   @Test
   fun deleteUserSuccess() = test {
     val testUser = postgres!!.testUser(admin = false)
     val client = createClient { install(ContentNegotiation) { json() } }
-    client
-      .delete("admin/users/${testUser.id}") {
+    val response =
+      client.delete("admin/users/${testUser.id}") {
         header("Cookie", sessionCookie(adminSession.sessionId))
       }
-      .apply { assertEquals(204, status.value) }
+
+    assertEquals(204, response.status.value)
   }
 
   @Test
   fun deleteUserFailsUserNotFound() = test {
     val client = createClient { install(ContentNegotiation) { json() } }
-    client
-      .delete("admin/users/${UUID.randomUUID()}") {
+    val response =
+      client.delete("admin/users/${UUID.randomUUID()}") {
         header("Cookie", sessionCookie(adminSession.sessionId))
       }
-      .apply {
-        assertEquals(404, status.value)
-        val body: AppError = body()!!
-        assertEquals(ErrorReason.EntityDoesNotExist, body.reason)
-      }
+
+    assertEquals(404, response.status.value)
+    val body: AppError = response.body()!!
+    assertEquals(ErrorReason.EntityDoesNotExist, body.reason)
   }
 }
