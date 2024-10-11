8b3d5d73fbd5ca683d0a1cfc9538dc19fa6e0a10
Antonio Vidaković <antonio.vidakovic@barrage.net>
2024-10-11 12:12:12 +0200

    Update database migration management to liquibase

--- setup.sh
+++ setup.sh
@@ -12,14 +12,21 @@ else
   cp gradle.example.properties gradle.properties
 fi
 
-
 # Set up the application properties
 cat ./config/application.yaml &> /dev/null
 
 if [ $? -eq 0 ]; then
   echo "application.yaml already exists, skipping"
-  exit 0;
 else
   cp ./config/application.example.yaml ./config/application.yaml
 fi
 
+# Set up the liquibase properties
+cat liquibase.properties &> /dev/null
+
+if [ $? -eq 0 ]; then
+  echo "liquibase.properties already exists, skipping"
+  exit 0;
+else
+  cp liquibase.example.properties liquibase.properties
+fi
---
+++ src/main/resources/db/migration/1_initial_migration/down.sql
@@ -0,0 +1,15 @@
+DROP TABLE failed_messages;
+
+DROP TABLE messages;
+
+DROP TABLE chats;
+
+DROP TABLE agent_instructions;
+
+DROP TABLE agent_collections;
+
+DROP TABLE agents;
+
+DROP TABLE sessions;
+
+DROP TABLE users;
