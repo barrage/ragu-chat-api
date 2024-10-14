df13ebb7ae7030f6d47974ba1e9a08b4adc43194
jbenkodakovic <josip.benkodakovic@barrage.net>
2024-10-14 15:28:50 +0200

    Add websocket end to end tests

--- src/main/kotlin/net/barrage/llmao/app/api/ws/Socket.kt
+++ src/main/kotlin/net/barrage/llmao/app/api/ws/Socket.kt
@@ -1,73 +1,106 @@
 package net.barrage.llmao.app.api.ws
 
+import io.ktor.http.*
 import io.ktor.serialization.kotlinx.*
 import io.ktor.server.application.*
 import io.ktor.server.auth.*
+import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import io.ktor.server.websocket.*
 import io.ktor.websocket.*
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.launch
-import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
+import net.barrage.llmao.core.types.KUUID
 import net.barrage.llmao.error.AppError
 import net.barrage.llmao.error.ErrorReason
+import net.barrage.llmao.plugins.queryParam
 import net.barrage.llmao.plugins.user
 
 fun Application.websocketServer(server: Server) {
   install(WebSockets) {
+    // FIXME: Shrink
     maxFrameSize = Long.MAX_VALUE
     masking = false
     contentConverter = KotlinxWebsocketSerializationConverter(ClientMessageSerializer)
   }
 
   routing {
+    // Protected WS route for one time tokens.
     authenticate("auth-session") {
-      webSocket {
-        // Validate session
-        val user = call.user()
+      route("/ws") {
+        get {
+          val user = call.user()
+          val token = server.registerToken(user.id)
+          call.respond(HttpStatusCode.OK, "$token")
+        }
+      }
+    }
 
-        // Handles connection concurrently
-        val messageResponseFlow = MutableSharedFlow<String>()
-        val sharedFlow = messageResponseFlow.asSharedFlow()
-        val job = launch { sharedFlow.collect { send(it) } }
+    webSocket {
+      // Validate session
+      val rawToken = call.queryParam("token")
 
-        val emitter = Emitter(messageResponseFlow)
+      if (rawToken == null) {
+        close(CloseReason(CloseReason.Codes.VIOLATED_POLICY, "Unauthorized"))
+        return@webSocket
+      }
 
-        for (frame in incoming) {
-          when (frame) {
-            is Frame.Text -> {
-              val message: ClientMessage
+      val token: KUUID
+      try {
+        token = KUUID.fromString(rawToken)
+      } catch (e: IllegalArgumentException) {
+        close(CloseReason(CloseReason.Codes.VIOLATED_POLICY, "Unauthorized"))
+        return@webSocket
+      }
 
-              try {
-                message = Json.decodeFromString(frame.readText())
-              } catch (e: Throwable) {
-                e.printStackTrace()
-                sendJson(AppError.api(ErrorReason.InvalidParameter, "Message format malformed"))
-                continue
-              }
+      val userId = server.removeToken(token)
 
-              server.handleMessage(user.id, message, emitter)
+      if (userId == null) {
+        close(CloseReason(CloseReason.Codes.VIOLATED_POLICY, "Unauthorized"))
+        return@webSocket
+      }
+
+      // Handles connection concurrently
+      val messageResponseFlow = MutableSharedFlow<String>()
+      val sharedFlow = messageResponseFlow.asSharedFlow()
+      val job = launch { sharedFlow.collect { send(it) } }
+
+      val emitter = Emitter(messageResponseFlow)
+
+      for (frame in incoming) {
+        when (frame) {
+          is Frame.Text -> {
+            val message: ClientMessage
+
+            try {
+              message = Json.decodeFromString(frame.readText())
+            } catch (e: Throwable) {
+              e.printStackTrace()
+              sendSerialized(AppError.api(ErrorReason.InvalidParameter, "Message format malformed"))
+              continue
             }
 
-            is Frame.Close -> {
-              job.cancel()
-              server.removeChat(user.id)
-              close(CloseReason(CloseReason.Codes.NORMAL, "Session closed"))
-              return@webSocket
+            try {
+              server.handleMessage(userId, message, emitter)
+            } catch (error: AppError) {
+              sendSerialized(error)
             }
+          }
 
-            else -> {
-              sendJson(AppError.api(ErrorReason.InvalidParameter, "Only text messages are allowed"))
-            }
+          is Frame.Close -> {
+            job.cancel()
+            server.removeChat(userId)
+          }
+
+          else -> {
+            sendSerialized(
+              AppError.api(ErrorReason.InvalidParameter, "Only text messages are allowed")
+            )
           }
         }
       }
     }
   }
-}
-
-suspend inline fun <reified T> WebSocketSession.sendJson(data: T) {
-  send(Json.encodeToString(data))
 }
