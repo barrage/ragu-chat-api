c572794f1c34aa2d179c9864bb36174040cad592
jbenkodakovic <josip.benkodakovic@barrage.net>
2024-11-21 17:02:42 +0100

    Improve the way user messages are formatted

--- src/test/kotlin/net/barrage/llmao/app/api/ws/WebsocketChatTests.kt
+++ src/test/kotlin/net/barrage/llmao/app/api/ws/WebsocketChatTests.kt
@@ -1,13 +1,8 @@
 package net.barrage.llmao.app.api.ws
 
-import io.ktor.client.*
-import io.ktor.client.plugins.websocket.*
-import io.ktor.client.request.*
-import io.ktor.client.statement.*
-import io.ktor.http.*
-import io.ktor.websocket.*
 import kotlin.random.Random
 import net.barrage.llmao.COMPLETIONS_STREAM_RESPONSE
+import net.barrage.llmao.COMPLETIONS_STREAM_RESPONSE_PROMPT
 import net.barrage.llmao.IntegrationTest
 import net.barrage.llmao.chatSession
 import net.barrage.llmao.core.models.Agent
@@ -23,9 +18,6 @@ import org.junit.jupiter.api.Test
 
 private const val TEST_COLLECTION = "KusturicaChatTests"
 
-/** Prompt configured to make wiremock return a stream response */
-private const val TEST_STREAM_PROMPT = "v1_chat_completions_stream"
-
 private const val SIZE = 1536
 
 class WebsocketChatTests :
@@ -34,29 +26,59 @@ class WebsocketChatTests :
     useWeaviate = true,
     // wiremockUrlOverride = "http://localhost:8080",
   ) {
-  private lateinit var agent: Agent
-  private lateinit var agentConfiguration: AgentConfiguration
   private lateinit var user: User
   private lateinit var session: Session
-  private lateinit var agentCollection: AgentCollection
+
+  private lateinit var validAgent: Agent
+  private lateinit var validAgentConfiguration: AgentConfiguration
+  private lateinit var validAgentCollection: AgentCollection
+
+  private lateinit var invalidAgent: Agent
+  private lateinit var invalidAgentConfiguration: AgentConfiguration
+  private lateinit var invalidAgentCollection: AgentCollection
 
   @BeforeAll
   fun setup() {
-    agent =
+    user = postgres!!.testUser(email = "not@important.org", admin = false)
+    session = postgres!!.testSession(user.id)
+
+    validAgent =
       postgres!!.testAgent(
         embeddingProvider = "azure",
         embeddingModel = "text-embedding-ada-002", // 1536
       )
-    agentConfiguration =
+    validAgentConfiguration =
       postgres!!.testAgentConfiguration(
-        agentId = agent.id,
+        agentId = validAgent.id,
         llmProvider = "openai",
         model = "gpt-4o",
       )
-    user = postgres!!.testUser(email = "not@important.org", admin = false)
-    session = postgres!!.testSession(user.id)
-    agentCollection =
-      postgres!!.testAgentCollection(agent.id, TEST_COLLECTION, 2, "You pass the butter")
+    validAgentCollection =
+      postgres!!.testAgentCollection(
+        agentId = validAgent.id,
+        collection = TEST_COLLECTION,
+        amount = 2,
+        instruction = "Use the valuable information below to solve the three body problem.",
+      )
+
+    invalidAgent =
+      postgres!!.testAgent(
+        embeddingProvider = "azure",
+        embeddingModel = "text-embedding-3-large", // 3072
+      )
+    invalidAgentConfiguration =
+      postgres!!.testAgentConfiguration(
+        agentId = invalidAgent.id,
+        llmProvider = "openai",
+        model = "gpt-4o",
+      )
+    invalidAgentCollection =
+      postgres!!.testAgentCollection(
+        invalidAgent.id,
+        TEST_COLLECTION,
+        2,
+        "Use the valuable information below to pass the butter.",
+      )
     insertTestVectors()
   }
 
@@ -66,18 +88,29 @@ class WebsocketChatTests :
    * A client connects via Websocket, opens a chat, sends a message and receives a response.
    */
   @Test
-  fun given_ValidAgentConfiguration_when_UserSendsMessage_then_AgentResponds() = wsTest { client ->
-    client.chatSession(session.sessionId) {
-      openNewChat(agent.id)
-      val response = sendMessage(TEST_STREAM_PROMPT)
-      assertEquals(COMPLETIONS_STREAM_RESPONSE, response)
+  fun given_ValidAgentConfiguration_when_UserSendsMessage_then_AgentRespondsWithInfoFromCollection() =
+    wsTest { client ->
+      client.chatSession(session.sessionId) {
+        openNewChat(validAgent.id)
+        val response = sendMessage("Will this trigger a stream response?")
+        assertEquals(COMPLETIONS_STREAM_RESPONSE, response)
+      }
     }
-  }
 
+  //  @Test
+  //  fun given_invalidAgentConfiguration_when_UserSendsMessage_then_ConfigurationErrorIsThrown() =
+  //    wsTest { client ->
+  //      client.chatSession(session.sessionId) {
+  //        openNewChat(invalidAgent.id)
+  //        val response = sendMessage("Will this trigger a stream response?")
+  //        assertEquals(COMPLETIONS_STREAM_RESPONSE, response)
+  //      }
+  //    }
+  //
   private fun insertTestVectors() {
-    val truthOne = Pair("Pee is stored in the balls.", List(SIZE) { Random.nextFloat() })
-    val truthTwo = Pair("It's flat, nice try globeheads.", List(SIZE) { Random.nextFloat() })
+    val streamResponsePrompt =
+      Pair(COMPLETIONS_STREAM_RESPONSE_PROMPT, List(SIZE) { Random.nextFloat() })
 
-    weaviate!!.insertVectors(TEST_COLLECTION, listOf(truthOne, truthTwo))
+    weaviate!!.insertVectors(TEST_COLLECTION, listOf(streamResponsePrompt))
   }
 }
