755c184d63ee55f08fd88c66f3c8e73e98d072ad
jbenkodakovic <josip.benkodakovic@barrage.net>
2024-11-22 10:22:33 +0100

    Add invalid embedding configuration test

--- src/test/kotlin/net/barrage/llmao/WebsocketTest.kt
+++ src/test/kotlin/net/barrage/llmao/WebsocketTest.kt
@@ -1,22 +1,18 @@
 package net.barrage.llmao
 
-import com.aallam.openai.api.core.FinishReason
 import io.ktor.client.*
 import io.ktor.client.plugins.websocket.*
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
 import io.ktor.http.*
-import io.ktor.serialization.kotlinx.*
 import io.ktor.websocket.*
-import kotlinx.serialization.SerializationException
+import kotlinx.coroutines.channels.ReceiveChannel
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 import net.barrage.llmao.app.api.ws.ServerMessage
 import net.barrage.llmao.app.api.ws.SystemMessage
 import net.barrage.llmao.core.types.KUUID
-import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertNotNull
-import org.junit.jupiter.api.Assertions.assertNull
 
 val json = Json { ignoreUnknownKeys = true }
 
@@ -44,41 +40,13 @@ suspend fun ClientWebSocketSession.openNewChat(agentId: KUUID) {
 }
 
 /** Send a chat message and wait for the response. */
-suspend fun ClientWebSocketSession.sendMessage(text: String): String {
+suspend fun ClientWebSocketSession.sendMessage(
+  text: String,
+  block: suspend (ReceiveChannel<Frame>) -> Unit,
+) {
   val message = "{ \"type\": \"chat\", \"text\": \"$text\" }"
   send(Frame.Text(message))
-
-  var buffer = ""
-
-  for (frame in incoming) {
-    val response = (frame as Frame.Text).readText()
-    try {
-      when (val finishEvent = json.decodeFromString<ServerMessage>(response)) {
-        is ServerMessage.FinishEvent -> {
-          assert(finishEvent.reason == FinishReason.Stop)
-          assertNull(finishEvent.content)
-          break
-        }
-
-        is ServerMessage.ChatTitle -> {
-          assertEquals(COMPLETIONS_TITLE_RESPONSE, finishEvent.title.isNotBlank())
-        }
-
-        else -> {}
-      }
-    } catch (e: SerializationException) {
-      val errMessage = e.message ?: throw e
-      if (!errMessage.startsWith("Expected JsonObject, but had JsonLiteral")) {
-        throw e
-      }
-      buffer += response
-    } catch (e: Throwable) {
-      e.printStackTrace()
-      break
-    }
-  }
-
-  return buffer
+  block(incoming)
 }
 
 inline fun <reified T> receiveJson(message: String): T = json.decodeFromString(message)
--- src/test/kotlin/net/barrage/llmao/app/api/ws/WebsocketChatTests.kt
+++ src/test/kotlin/net/barrage/llmao/app/api/ws/WebsocketChatTests.kt
@@ -1,6 +1,9 @@
 package net.barrage.llmao.app.api.ws
 
+import com.aallam.openai.api.core.FinishReason
+import io.ktor.websocket.*
 import kotlin.random.Random
+import kotlinx.serialization.SerializationException
 import net.barrage.llmao.COMPLETIONS_STREAM_RESPONSE
 import net.barrage.llmao.COMPLETIONS_STREAM_RESPONSE_PROMPT
 import net.barrage.llmao.IntegrationTest
@@ -10,9 +13,14 @@ import net.barrage.llmao.core.models.AgentCollection
 import net.barrage.llmao.core.models.AgentConfiguration
 import net.barrage.llmao.core.models.Session
 import net.barrage.llmao.core.models.User
+import net.barrage.llmao.error.AppError
+import net.barrage.llmao.error.ErrorReason
+import net.barrage.llmao.json
 import net.barrage.llmao.openNewChat
 import net.barrage.llmao.sendMessage
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertNull
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
 
@@ -44,7 +52,7 @@ class WebsocketChatTests :
 
     validAgent =
       postgres!!.testAgent(
-        embeddingProvider = "azure",
+        embeddingProvider = "openai",
         embeddingModel = "text-embedding-ada-002", // 1536
       )
     validAgentConfiguration =
@@ -63,7 +71,7 @@ class WebsocketChatTests :
 
     invalidAgent =
       postgres!!.testAgent(
-        embeddingProvider = "azure",
+        embeddingProvider = "openai",
         embeddingModel = "text-embedding-3-large", // 3072
       )
     invalidAgentConfiguration =
@@ -86,27 +94,71 @@ class WebsocketChatTests :
    * Tests if the whole application flow works as expected.
    *
    * A client connects via Websocket, opens a chat, sends a message and receives a response.
+   * Beforehand, the agent is configured to the right collection.
    */
   @Test
-  fun given_ValidAgentConfiguration_when_UserSendsMessage_then_AgentRespondsWithInfoFromCollection() =
-    wsTest { client ->
-      client.chatSession(session.sessionId) {
-        openNewChat(validAgent.id)
-        val response = sendMessage("Will this trigger a stream response?")
-        assertEquals(COMPLETIONS_STREAM_RESPONSE, response)
+  fun worksWhenAgentIsConfiguredProperly() = wsTest { client ->
+    client.chatSession(session.sessionId) {
+      openNewChat(validAgent.id)
+
+      var asserted = false
+      var buffer = ""
+
+      sendMessage("Will this trigger a stream response?") { incoming ->
+        for (frame in incoming) {
+          val response = (frame as Frame.Text).readText()
+          try {
+            val finishEvent = json.decodeFromString<ServerMessage>(response)
+
+            // React only to finish events since titles can also get sent
+            if (finishEvent is ServerMessage.FinishEvent) {
+              assert(finishEvent.reason == FinishReason.Stop)
+              assertNull(finishEvent.content)
+              asserted = true
+              break
+            }
+          } catch (e: SerializationException) {
+            val errMessage = e.message ?: throw e
+            if (!errMessage.startsWith("Expected JsonObject, but had JsonLiteral")) {
+              throw e
+            }
+            buffer += response
+          } catch (e: Throwable) {
+            e.printStackTrace()
+            break
+          }
+        }
+      }
+
+      assertEquals(COMPLETIONS_STREAM_RESPONSE, buffer)
+      assert(asserted)
+    }
+  }
+
+  /**
+   * A client connects via Websocket, opens a chat, sends a message and receives an error.
+   * Beforehand, the agent is configured to the wrong collection (whose size is different than the
+   * embeddings created by the agent's model).
+   */
+  @Test
+  fun sendsVectorDatabaseErrorWhenAgentEmbeddingModelIsNotConfiguredProperly() = wsTest { client ->
+    var asserted = false
+    client.chatSession(session.sessionId) {
+      openNewChat(invalidAgent.id)
+      sendMessage("Will this trigger a stream response?") { incoming ->
+        for (frame in incoming) {
+          val response = (frame as Frame.Text).readText()
+          val error = json.decodeFromString<AppError>(response)
+          assertEquals(ErrorReason.VectorDatabase, error.reason)
+          assertTrue(error.description!!.contains("vector lengths don't match"))
+          asserted = true
+          break
+        }
       }
     }
+    assert(asserted)
+  }
 
-  //  @Test
-  //  fun given_invalidAgentConfiguration_when_UserSendsMessage_then_ConfigurationErrorIsThrown() =
-  //    wsTest { client ->
-  //      client.chatSession(session.sessionId) {
-  //        openNewChat(invalidAgent.id)
-  //        val response = sendMessage("Will this trigger a stream response?")
-  //        assertEquals(COMPLETIONS_STREAM_RESPONSE, response)
-  //      }
-  //    }
-  //
   private fun insertTestVectors() {
     val streamResponsePrompt =
       Pair(COMPLETIONS_STREAM_RESPONSE_PROMPT, List(SIZE) { Random.nextFloat() })
