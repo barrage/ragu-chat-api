a4eb078ca0905b17cb68dd77b01f9fda63a052eb
jbenkodakovic <josip.benkodakovic@barrage.net>
2024-11-26 15:15:58 +0100

    Improve queries to Weaviate

--- src/main/kotlin/net/barrage/llmao/core/vector/VectorDatabase.kt
+++ src/main/kotlin/net/barrage/llmao/core/vector/VectorDatabase.kt
@@ -4,10 +4,13 @@ interface VectorDatabase {
   fun id(): String
 
   /**
-   * Perform semantic similarity search in the given `collection`, returning `amount` most similar
-   * results.
+   * Perform semantic similarity search in the given `collections`, returning `amount` most similar
+   * results from each based on the given `searchVector`.
    */
-  fun query(searchVector: List<Double>, collection: String, amount: Int): List<String>
+  fun query(
+    searchVector: List<Double>,
+    collections: List<Pair<String, Int>>,
+  ): Map<String, List<VectorData>>
 
   /**
    * Used by services to reason about the existence of collections. Returns `true` if the collection
@@ -15,3 +18,12 @@ interface VectorDatabase {
    */
   fun validateCollection(name: String, vectorSize: Int): Boolean
 }
+
+/** Data class representing the payload associated with a vector. */
+data class VectorData(
+  /** The original document (or chunk) content. */
+  val content: String,
+
+  /** The document ID in `chonkit`. */
+  val documentId: String?,
+)
