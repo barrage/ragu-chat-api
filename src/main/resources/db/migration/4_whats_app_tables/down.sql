a181dc5ac03ee72c933ce6c92b629cfacb40c78e
Dino Bajramovic <dino@barrage.net>
2024-12-13 11:36:32 +0100

    Update migrations

--- src/main/resources/db/migration/4_whats_app_tables/down.sql
+++ src/main/resources/db/migration/4_whats_app_tables/down.sql
@@ -1,3 +1,7 @@
+DROP INDEX IF EXISTS idx_whatsapp_numbers_user_id;
+DROP INDEX IF EXISTS idx_whatsapp_numbers_phone_number;
+DROP INDEX IF EXISTS idx_whatsapp_agents_active;
+
 DROP TRIGGER IF EXISTS set_updated_at ON whats_app_messages;
 DROP TRIGGER IF EXISTS set_updated_at ON whats_app_chats;
 DROP TRIGGER IF EXISTS set_updated_at ON whats_app_agent_collections;
--- src/main/resources/db/migration/6_add_prompt_instruction_to_agent_configurations/down.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE agent_configurations DROP COLUMN IF EXISTS prompt_instruction;
-
-ALTER TABLE whats_app_agents DROP COLUMN IF EXISTS prompt_instruction;
