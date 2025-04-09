a9d9d101467c0bb8e30f62b77f85837ad66e93ca
jbenkodakovic <josip.benkodakovic@barrage.net>
2025-04-09 13:51:11 +0200

    Add initial vLLM implementation

--- src/main/kotlin/net/barrage/llmao/app/llm/LlmProviderFactory.kt
+++ src/main/kotlin/net/barrage/llmao/app/llm/LlmProviderFactory.kt
@@ -2,7 +2,7 @@ package net.barrage.llmao.app.llm
 
 import io.ktor.server.config.*
 import net.barrage.llmao.app.llm.openai.AzureAI
-import net.barrage.llmao.app.llm.openai.OpenAI
+import net.barrage.llmao.app.llm.openai.Vllm
 import net.barrage.llmao.core.ProviderFactory
 import net.barrage.llmao.core.llm.LlmProvider
 import net.barrage.llmao.string
@@ -24,13 +24,18 @@ class LlmProviderFactory(config: ApplicationConfig) : ProviderFactory<LlmProvide
         providers["ollama"] = initOllama(config)
       }
     }
+    config.tryGetString("ktor.features.llm.vllm")?.toBoolean()?.let { enabled ->
+      if (enabled) {
+        providers["vllm"] = initVllm(config)
+      }
+    }
   }
 
-  private fun initOpenAi(config: ApplicationConfig): OpenAI {
+  private fun initOpenAi(config: ApplicationConfig): Vllm {
     val endpoint = config.string("llm.openai.endpoint")
     val apiKey = config.string("llm.openai.apiKey")
 
-    return OpenAI(endpoint, apiKey)
+    return Vllm(endpoint, apiKey)
   }
 
   private fun initAzure(config: ApplicationConfig): AzureAI {
@@ -45,5 +50,12 @@ class LlmProviderFactory(config: ApplicationConfig) : ProviderFactory<LlmProvide
     val endpoint = config.string("llm.ollama.endpoint")
 
     return Ollama(endpoint)
+  }
+
+  private fun initVllm(config: ApplicationConfig): Vllm {
+    val endpoint = config.string("llm.vllm.endpoint")
+    val apiKey = config.string("llm.vllm.apiKey")
+
+    return Vllm(endpoint, apiKey)
   }
 }
---
+++ src/main/kotlin/net/barrage/llmao/app/llm/openai/Vllm.kt
@@ -0,0 +1,74 @@
+package net.barrage.llmao.app.llm.openai
+
+import com.aallam.openai.api.chat.ChatCompletionRequest
+import com.aallam.openai.api.chat.StreamOptions
+import com.aallam.openai.api.logging.LogLevel
+import com.aallam.openai.api.model.ModelId
+import com.aallam.openai.client.LoggingConfig
+import com.aallam.openai.client.OpenAI
+import com.aallam.openai.client.OpenAIHost
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import net.barrage.llmao.core.llm.ChatCompletion
+import net.barrage.llmao.core.llm.ChatCompletionParameters
+import net.barrage.llmao.core.llm.ChatMessage
+import net.barrage.llmao.core.llm.ChatMessageChunk
+import net.barrage.llmao.core.llm.LlmProvider
+
+private val SUPPORTED_MODELS = listOf("Qwen/Qwen2.5-1.5B-Instruct")
+
+class Vllm(endpoint: String, apiKey: String) : LlmProvider {
+  private val client: OpenAI =
+    OpenAI(
+      token = apiKey,
+      host = OpenAIHost(endpoint),
+      logging = LoggingConfig(logLevel = LogLevel.Info),
+    )
+
+  override fun id(): String {
+    return "vllm"
+  }
+
+  override suspend fun chatCompletion(
+    messages: List<ChatMessage>,
+    config: ChatCompletionParameters,
+  ): ChatCompletion {
+    val chatRequest =
+      ChatCompletionRequest(
+        messages = messages.map { it.toOpenAiChatMessage() },
+        model = ModelId(config.model),
+        temperature = config.temperature,
+        presencePenalty = config.presencePenalty,
+        maxTokens = config.maxTokens,
+        tools = config.tools?.map { it.toOpenAiTool() },
+      )
+
+    return client.chatCompletion(chatRequest).toNativeChatCompletion()
+  }
+
+  override suspend fun completionStream(
+    messages: List<ChatMessage>,
+    config: ChatCompletionParameters,
+  ): Flow<ChatMessageChunk> {
+    val chatRequest =
+      ChatCompletionRequest(
+        messages = messages.map { it.toOpenAiChatMessage() },
+        streamOptions = StreamOptions(true),
+        model = ModelId(config.model),
+        temperature = config.temperature,
+        presencePenalty = config.presencePenalty,
+        maxTokens = config.maxTokens,
+        tools = config.tools?.map { it.toOpenAiTool() },
+      )
+
+    return this.client.chatCompletions(chatRequest).map { chunk -> chunk.toNativeMessageChunk() }
+  }
+
+  override suspend fun supportsModel(model: String): Boolean {
+    return SUPPORTED_MODELS.contains(model)
+  }
+
+  override suspend fun listModels(): List<String> {
+    return SUPPORTED_MODELS
+  }
+}
